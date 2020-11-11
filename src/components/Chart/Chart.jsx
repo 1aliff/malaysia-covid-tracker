70c8bc1b72fe75a2fdc4632ab8f719cd0d628c81
1aliff <xy.aliff@gmail.com>
2020-11-11 15:03:17 +0800

    Added typography for chart

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,6 +1,6 @@
 import {BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend} from 'recharts';
 
-import { Grid } from '@material-ui/core';
+import { Grid, Typography } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
@@ -20,13 +20,18 @@ function Chart(props) {
     const { data : { recovered, confirmed, active }} = props
 
     const chartData = [
-        {name: 'Confirmed vs Recovered vs Active', recovered: recovered, confirmed: confirmed, active: active },
+        {
+            recovered: recovered, 
+            confirmed: confirmed, 
+            active: active 
+        },
     ];
 
     return (
         <div className={classes.root}>   
             <Grid container spacing={3}>
                 <Grid item xs={12} sm={6}>
+                    <Typography variant="h5" align="center">Active vs Recovered vs Death</Typography>
                     <BarChart width={800} height={450} data={chartData}>
                     <CartesianGrid strokeDasharray="3 3"/>
                     <XAxis dataKey="name"/>
