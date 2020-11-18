9079a6340227c836964232b9f2aea21bc89d943f
1aliff <xy.aliff@gmail.com>
2020-11-18 15:36:14 +0800

    Implement button for charts. Set hooks

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,6 +1,8 @@
+import React, { useState } from 'react';
+
 import {BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend} from 'recharts';
 
-import { Grid, Typography } from '@material-ui/core';
+import { Grid, Typography, Button} from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
@@ -16,9 +18,15 @@ const useStyles = makeStyles((theme) => ({
 
 function Chart(props) {
     const classes = useStyles();
+    
+    // state hooks
+    const [displayChart, setDisplayChart] = useState(false)
+    const [buttonTitle, setButtonTitle] = useState('View in Chart')
 
+    //decons data from props
     const { data : { recovered, confirmed, active }} = props
 
+    // assign data for recharts
     const chartData = [
         {
             recovered: recovered, 
@@ -26,24 +34,38 @@ function Chart(props) {
             active: active 
         },
     ];
+    
+    function handleClick() {
+        setDisplayChart(!displayChart)
+        !displayChart ? setButtonTitle('Close chart') : setButtonTitle('View in Chart')
+
+    }
 
     return (
-        <div className={classes.root}>   
-            <Grid container spacing={3}>
-                <Grid item xs={12} sm={6}>
-                    <Typography variant="h5" align="center">Active vs Recovered vs Death</Typography>
-                    <BarChart width={800} height={450} data={chartData}>
-                    <CartesianGrid strokeDasharray="3 3"/>
-                    <XAxis dataKey="name"/>
-                    <YAxis/>
-                    <Tooltip/>
-                    <Legend />
-                    <Bar dataKey="confirmed" fill="#5B8FF8" />
-                    <Bar dataKey="recovered" fill="#339933" />
-                    <Bar dataKey="active" fill="#ff0000" />
-                    </BarChart>
-                </Grid>
+        <div className={classes.root}>
+            <Grid container justify="center">
+                <Button variant="contained" color="primary" onClick={handleClick}>{buttonTitle}</Button>
             </Grid>
+
+            { displayChart ? 
+                <Grid container justify="center">
+                    <Grid item xs={12} sm={6}>
+                    <br /><br />
+                    <Typography variant="h5" align="center">Current Malaysia Covid 19 Cases</Typography>
+                        <BarChart width={800} height={450} data={chartData}>
+                        <CartesianGrid strokeDasharray="3 3"/>
+                        <XAxis dataKey="name"/>
+                        <YAxis/>
+                        <Tooltip/>
+                        <Legend />
+                        <Bar dataKey="confirmed" fill="#5B8FF8" />
+                        <Bar dataKey="recovered" fill="#339933" />
+                        <Bar dataKey="active" fill="#ff0000" />
+                        </BarChart>
+                    </Grid>
+                </Grid> : null 
+            }
+
         </div>
     )
 }
